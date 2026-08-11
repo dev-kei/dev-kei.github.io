51da393882c3f9eb032951310995dedd5f4d6885
dev-kei <official.keikish@gmail.com>
2026-08-12 00:27:14 +0800

    collective update

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,35 @@ $(document).ready(function() {
       $("#case_studies").mouseleave(function(){
         $("#case_studies").hide(); 
       });
+
+    });
+
+$(document).ready(function () {
+
+    $('#sidenav').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+    $('#case_studies').hide();
+
+    $('.case').mouseenter(function () {
+        $('#case_studies').show();
+    });
+
+    $('#case_studies').mouseleave(function () {
+        $('#case_studies').hide();
+    });
+
+
+    // Trusted companies carousel
+    const track = document.querySelector('.logo-track');
+
+    if (track) {
+        const logos = [...track.children];
+
+        logos.forEach(logo => {
+            track.appendChild(logo.cloneNode(true));
+        });
+    }
+
 });
